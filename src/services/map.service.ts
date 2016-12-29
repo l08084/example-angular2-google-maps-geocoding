d964e7b15922062b3f66afc0822c6b1ba6886885
l08084 <l08084@shibaura-it.ac.jp>
2016-12-29 15:32:01 +0900

    test

--- src/services/map.service.ts
+++ src/services/map.service.ts
@@ -15,6 +15,7 @@ export class MapService {
     public geocoding(address: string): Promise<any> {
         return this.mapsAPILoader.load().then(() => {
             this.geocoder = new google.maps.Geocoder();
+
             return new Promise((resolve, reject) => {
                 this.geocoder.geocode({ 'address': address }, (result: any, status: any) => {
                     if (status === google.maps.GeocoderStatus.OK) {
